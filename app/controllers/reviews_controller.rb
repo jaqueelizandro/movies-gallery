102aaf6be7cdded690cc2cf94f6a4129565f2351
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-12 11:31:59 +1000

    delet review working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,36 +1,45 @@
 class ReviewsController < ApplicationController
+  before_action :find_movie
+  before_action :find_review, :only => [:edit, :update, :destroy]
   
   def index
   end
 
   def new
-    @movie = Movie.find params[:movie_id]
     @review = @movie.reviews.build
   end
 
   def create
-    movie = Movie.find params[:movie_id]
-    movie.reviews.create(review_params)
-    redirect_to movie_path(movie.imdbid)
+    @movie.reviews.create(review_params)
+    redirect_to movie_path(@movie.imdbid)
   end
 
   def edit
-    @movie = Movie.find params[:movie_id]
-    @review = Review.find params[:id]
   end
 
   def update
-    movie = Movie.find params[:movie_id]
-    review = Review.find params[:id]
-    review.update(review_params)
-    redirect_to movie_path(movie.imdbid)
+    @review.update(review_params)
+    redirect_to movie_path(@movie.imdbid)
   end
 
   def show
   end
 
+  def destroy
+    @review.destroy
+    redirect_to movie_path(@movie.imdbid)
+  end
+
   private
   def review_params
     params.require(:review).permit(:rate, :review, :user_id, :movie_id)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-    belongs_to :movie, dependent: :destroy, optional: true
-    belongs_to :user, dependent: :destroy, optional: true
+    belongs_to :movie, :optional => :true
+    belongs_to :user, :optional => :true
 end
