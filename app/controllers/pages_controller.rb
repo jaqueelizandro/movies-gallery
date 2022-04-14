39bfb7cd02597dba0b0909f411cda16f13c232ea
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-14 16:48:10 +1000

    css styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
     def home
         @movies = Movie.order(created_at: :desc).limit(5)
+        # @reviews = Review.all.map(&:movie_id).tally
     end
 end
