1b1f2176121e7e2ea148ef69a23828d11826b501
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-15 20:59:10 +1000

    message for wrong email or password log in

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,18 +1,17 @@
 class SessionController < ApplicationController
     def new
-      @user = User.find_by :email => params[:email]
     end
   
     def create
-      user = User.find_by :email => params[:email]
-        if user.present? && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect_to root_path
-      else
-        flash.now[:error] = 'Wrong login or password'
-        render 'new'
-        # redirect_to new_user_path
-      end
+        user = User.find_by :email => params[:email]
+            if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            flash.now[:message] = "Wrong email or password. Try it again!"
+            render 'new'
+            # redirect_to new_user_path
+        end
     end
   
     def destroy
