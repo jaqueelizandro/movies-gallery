2749090eeaf4e33693d07b23edfe7a5da5e0b554
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-15 20:59:49 +1000

    css styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_10_080413) do
+ActiveRecord::Schema.define(version: 2022_04_15_073546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
