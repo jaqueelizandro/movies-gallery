b5f19ab99eb25b042a94e211ac4eab2151661742
Jaque Elizandro <jaque@elizandro.com.br>
2022-04-18 16:34:46 +1000

    recommended system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
     def home
         @movies = Movie.order(created_at: :desc).limit(5)
-        # @reviews = Review.all.map(&:movie_id).tally
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
-    has_many :reviews, :dependent => :destroy
+    # has_many :reviews, :dependent => :destroy
+    has_many :reviews
     has_many :users, :through => :reviews
 end
+
+
